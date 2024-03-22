703d3852715432ebe7e43a215f415581053d349c
QueenLinlay <leejusti217@gmail.com>
2024-03-22 10:40:52 -0400

    Update

--- Assets/Team #3 Scripts/BlockerAgent.cs
+++ Assets/Team #3 Scripts/BlockerAgent.cs
@@ -44,6 +44,8 @@ public class BlockerAgent : Agent
     }
 
     public float forceMultiplier = 5f;
+    public float forceJump = 10f;
+    public bool jumpIsReady = true;
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         // Actions, size = 2
@@ -53,7 +55,7 @@ public class BlockerAgent : Agent
         float moveZ = actionBuffers.DiscreteActions[1];
         //controlSignal.y = actionBuffers.ContinuousActions[1]; // 0 = dont move; 1 = back; 2 = forward
         float moveY = actionBuffers.DiscreteActions[2];
-        //controlSignal.z = actionBuffers.ContinuousActions[2]; // 0 = dont move; 1 = down; 2 = up;
+        //controlSignal.z = actionBuffers.ContinuousActions[2] still defining - 0 = do nothing; 1 = jump once;
         //rBody.AddForce(controlSignal * forceMultiplier);
         Vector3 addForce = new Vector3(0, 0, 0);
 
@@ -73,9 +75,7 @@ public class BlockerAgent : Agent
 
         switch (moveY)
         {
-            case 0: addForce.y = 0f; break;
-            case 1: addForce.y = -0.5f; break;
-            case 2: addForce.y = +0.5f; break;
+            case 1: Jump();  Debug.Log("Move Y = 1"); break;
         }
 
         //Speed
@@ -84,10 +84,16 @@ public class BlockerAgent : Agent
         // Rewards
         float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
 
+        if (this.transform.localPosition.y > -0.678)
+        {
+            AddReward(-0.1f);
+            EndEpisode();
+        }
+
         // Reached target
         if (distanceToTarget < 0.42f)
         {
-            SetReward(1.0f);
+            SetReward(0.2f);
             EndEpisode();
         }
 
@@ -96,6 +102,7 @@ public class BlockerAgent : Agent
         {
             EndEpisode();
         }
+
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -103,5 +110,44 @@ public class BlockerAgent : Agent
         var continuousActionsOut = actionsOut.ContinuousActions;
         continuousActionsOut[0] = Input.GetAxis("Horizontal");
         continuousActionsOut[1] = Input.GetAxis("Vertical");
+
+
+    }
+
+
+    // When collider is enter (the ball that is shooting into the net) do something (can affect others collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Add this to main script later for the basketball
+        if (collision.gameObject.tag == "Floor")
+        {
+            jumpIsReady = true;
+        }
+
+        if (collision.gameObject.tag == "Basketball")
+        {
+            //spawn.Check = false;
+            Destroy(collision.gameObject);
+            Debug.Log("AI Collide with BasketBall Reward");
+            AddReward(1.0f);
+            EndEpisode();
+            //Instantiate(ball, new Vector3(0f, 1f, 0f), Quaternion.identity);
+        }
+        else
+        {
+
+            Debug.Log("AI GameObject has collide with other GameObject... will not destroy them..");
+            //AddReward(-0.1f);
+            //EndEpisode();
+        }
+
+    }
+    void Jump()
+    {
+        if (jumpIsReady)
+        {
+            rBody.AddForce(Vector3.up * forceJump, ForceMode.Impulse);
+                jumpIsReady = false;
+        }
     }
 }
